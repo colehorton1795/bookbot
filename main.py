b434a35337266608611688ea3507babf548b7c95
colehorton1795 <colehorton1795@gmail.com>
2026-01-16 14:51:56 -0800

    B: word count update

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ def main():
 	text = get_book_text(book_path)
 	print(text)
 
-
-
 def get_book_text(path):
 	with open(path) as f:
 		return f.read()
-	
 
+def word_count(text):
+    wordnum = text.split()
+    return len(wordnum)	
+
+    
 main()
+
+
+
+
