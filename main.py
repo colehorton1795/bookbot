402afb5ad2e83a1d444a3ae94810a67891470540
colehorton1795 <colehorton1795@gmail.com>
2026-01-16 21:40:02 -0800

    C: update

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 def main():
 	book_path = "books/frankenstein.txt"
 	text = get_book_text(book_path)
-	print(text)
+	num_words = get_num_words(text)
+	print(f"Found {num_words} total words")
+	
 
 def get_book_text(path):
 	with open(path) as f:
 		return f.read()
 
-def word_count(text):
-    wordnum = text.split()
-    return len(wordnum)	
+def get_num_words(text):
+    words = text.split()
+    return len(words)	
 
     
 main()
